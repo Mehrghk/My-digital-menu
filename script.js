62a4321c930b492e3213721992c93dcf79057d43
Mehrghk <165900437+Mehrghk@users.noreply.github.com>
2025-01-20 22:19:57 +0330

    Add files via upload

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ clock.addEventListener('mouseover', function(){
     clock_hours_body.classList.add('visible');
     // clock_hours_body.style.display = 'block';
     // outer_div_nav_options.style.display = 'flex';
-
+    outer_div_nav_options.classList.add('visible');
 });
 clock.addEventListener('mouseout', function(e){
     timer = setTimeout(() => {
@@ -46,6 +46,7 @@ clock.addEventListener('mouseout', function(e){
             clock_hours_body.style.margin = '0px 0px';
             clock_hours_body.style.transition = '.3s';
             // clock_hours_body.style.display = 'none';
+            outer_div_nav_options.classList.remove('visible');
         }
     }, 100);
         
@@ -59,6 +60,8 @@ clock_hours_body.addEventListener('mouseout', function() {
             clock_hours_body.style.margin = '0px 0px';
             clock_hours_body.style.transition = '.3s';
             clock_hours_body.classList.remove('visible');
+            outer_div_nav_options.classList.remove('visible');
+
             // clock_hours_body.style.display = 'none';
             // outer_div_nav_options.style.display = 'none';
         }
@@ -73,7 +76,8 @@ list.addEventListener('mouseover', function(){
     list_body.classList.add('visible');
     // list_body.style.display = 'block';
     // outer_div_nav_options.style.display = 'flex';
-    outer_div_nav_options.style.z_index = '10';
+    // outer_div_nav_options.style.z_index = '10';
+    outer_div_nav_options.classList.add('visible');
 
 });
 list.addEventListener('mouseout', function(e){
@@ -84,7 +88,8 @@ list.addEventListener('mouseout', function(e){
             list_body.style.transition = '.3s';
             // list_body.style.display = 'none';
             // outer_div_nav_options.style.display = 'none';
-            outer_div_nav_options.style.z_index = '-3';
+            // outer_div_nav_options.style.z_index = '-3';
+            outer_div_nav_options.classList.remove('visible');
 
         }
     }, 100);   
@@ -101,7 +106,8 @@ list_body.addEventListener('mouseout', function() {
             list_body.classList.remove('visible');
             // list_body.style.display = 'none';
             // outer_div_nav_options.style.display = 'none';
-            outer_div_nav_options.style.z_index = '-3';
+            // outer_div_nav_options.style.z_index = '-3';
+            outer_div_nav_options.classList.remove('visible');
 
         }
     }, 100);
@@ -112,3 +118,28 @@ document.addEventListener('wheel', (event) => {
     const container = document.querySelector('.scroll-container');
     container.scrollLeft += event.deltaY; // اسکرول افقی با موس
 });
+window.addEventListener('load', function () {
+    var path = window.location.pathname;
+    var fileName = path.substring(path.lastIndexOf('/') + 1);
+  
+    if (fileName === "restaurant-page.html") {
+        if (window.matchMedia("(max-width: 760px)").matches) {
+            alert('میتونید از زیر منوی سفید به چپ بکشید و عکس هارو ببینید!');
+        } else {
+            alert('در اینجا میتوانید عکس هایی از رستوران ما را ببینید!');
+        }
+    } else if (fileName === "index.html") {
+      alert('در حال حاظر تنها صفحات منو و رستوران آماده هستند!');
+    } 
+    // else if (fileName === "menu-page.html"){
+    //     if (window.matchMedia("(max-width: 1170px)").matches) {
+    //         alert('به منوی رستوران خوش آمدید');
+    //         } else if(window.matchMedia("(max-width: 990px)").matches) {
+    //             alert('صفحه به اندازه یک گوشی کوچک است!');
+    //         } else if(window.matchMedia("(max-width: 760px)").matches) {
+    //             alert('صفحه به اندازه یک گوشی کوچک است!');
+    //         } else if(window.matchMedia("(max-width: 500px)").matches) {
+    //             alert('صفحه به اندازه یک گوشی کوچک است!');
+    //         }
+    // }
+  });
